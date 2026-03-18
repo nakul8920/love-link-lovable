495f30416c59b237005216f4c0a0fd32f5bafef6
Nakul Jangra <nakuljangid99@gmail.com>
2026-03-18 17:34:21 +0530

    changes made

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,2 @@
 export const API_BASE_URL =
-  import.meta.env.VITE_API_URL ||
-  (window.location.hostname === "localhost" ||
-  window.location.hostname === "127.0.0.1"
-    ? "http://localhost:5000"
-    : `${window.location.protocol}//${window.location.hostname}:5000`);
+  import.meta.env.VITE_API_URL || "https://love-link-lovable.onrender.com";
